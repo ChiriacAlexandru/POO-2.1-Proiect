a18cabce9d49ea44f71e8187e395772a1ba1a840
Alina-Rodica-Valentina Copil <alina.copil@student.upt.ro>
2025-01-07 20:05:20 +0200

    Clasa Utilizator

--- POO-2.1-Proiect v2/Program.cs
+++ POO-2.1-Proiect v2/Program.cs
@@ -1,4 +1,1 @@
-﻿Console.WriteLine("TestV2");
-int a = 10;
-int b = 20;
-int c = a + b;
+﻿
---
+++ POO-2.1-Proiect v2/Utilizator.cs
@@ -0,0 +1,19 @@
+﻿
+public class Utilizator
+{
+    public string Nume;
+    public string Prenume;
+    public string Email;
+    public string Parola;
+    public bool IsAdmin;
+    public Utilizator(string nume, string prenume, string email,string parola,bool isAdmin)
+    {
+        Nume= nume;
+        Prenume= prenume;
+        Email= email;
+        Parola = parola;
+        IsAdmin= isAdmin;
+
+    }
+
+}
