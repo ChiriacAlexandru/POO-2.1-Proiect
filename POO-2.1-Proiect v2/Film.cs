04b7b48b52338bbdec991ddd68f46610354ede4a
Alina-Rodica-Valentina Copil <alina.copil@student.upt.ro>
2025-01-11 23:10:23 +0200

    Clasa rezervare,film incomplete

---
+++ POO-2.1-Proiect v2/Film.cs
@@ -0,0 +1,16 @@
+﻿public class Film
+{
+    public int Id;
+    public string Nume;
+    public DateTime DataStart;
+    public DateTime DataSfarsit;
+    public int SalaId;
+    public Film(int id, string nume, DateTime dataStart, DateTime dataSfarsit, int salaId)
+    {
+        Id = id;
+        Nume = nume;
+        DataStart = dataStart;
+        DataSfarsit = dataSfarsit;
+        SalaId = salaId;
+    }
+}
---
+++ POO-2.1-Proiect v2/Rezervare.cs
@@ -0,0 +1,12 @@
+﻿public class Rezervare
+{
+    public int FilmId;
+    public string NumeClient;
+    public int NumarLocuri;
+    public Rezervare(int filmId, string numeClient, int numarLocuri)
+    {
+        FilmId = filmId;
+        NumeClient = numeClient;
+        NumarLocuri = numarLocuri;
+    }
+}
