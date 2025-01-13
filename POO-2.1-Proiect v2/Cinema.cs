09a72a0bb52d363f11c24dbe2fafe16c784edad0
Denisa-Maria Ciuculescu <denisa.ciuculescu@student.upt.ro>
2025-01-13 15:50:53 +0200

    Clasa cinema completa

--- POO-2.1-Proiect v2/Cinema.cs
+++ POO-2.1-Proiect v2/Cinema.cs
@@ -22,6 +22,46 @@ namespace POO_2._1_Proiect_v2
         {
             File.WriteAllLines(fisier, Sali.Select(s => s.ToString()));
         }
-       
+        public void AdaugaFilm(Film film)
+        {
+            if (!Filme.Any(f => f.SalaId == film.SalaId && ((film.DataStart >= f.DataStart && film.DataStart < f.DataSfarsit) || (film.DataSfarsit > f.DataStart && film.DataSfarsit <= f.DataSfarsit))))
+            {
+                Filme.Add(film);
+            }
+            else
+                throw new Exception("Sala este deja rezervata pentru aceasta perioada.");
+        }
+
+
+        public void AdaugareRezervare(Rezervare rezervare)
+        {
+            var film = Filme.FirstOrDefault(f => f.Id == rezervare.FilmId);
+            if(film != null)
+            {
+                var sala=Sali.FirstOrDefault(s=>s.Id == film.SalaId);
+                if (sala != null)
+                {
+                    int locuriRezervate = Rezervari.Where(r => r.FilmId == rezervare.FilmId).Sum(r => r.NumarLocuri);
+                    if (locuriRezervate + rezervare.NumarLocuri <= sala.Capacitate)
+                    {
+                        Rezervari.Add(rezervare);
+                    }
+                    else
+                    {
+                        throw new Exception("Numarul de locuri rezervate depaseste capacitatea salii.");
+                  
+                   }
+                }
+                else 
+                {
+                   throw new Exception("Sala nu exista.");
+                }
+            }
+            else
+            {
+                throw new Exception("Filmul nu exista.");
+            }
+        }
+
     }
 }
