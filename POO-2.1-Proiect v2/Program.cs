7c3f09a29a548375099317d16fdfe9a31273c0fb
ChiriacAlexandru <119078036+ChiriacAlexandru@users.noreply.github.com>
2025-01-15 15:59:11 +0200

    Update Program.cs
    clasa program, citire fisiere

--- POO-2.1-Proiect v2/Program.cs
+++ POO-2.1-Proiect v2/Program.cs
@@ -1,1 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CinemaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cinema = new Cinema();
+            string saliFisier = "sali.txt";
+            string filmeFisier = "filme.txt";
+            string utilizatoriFisier = "utilizatori.txt";
+
+            cinema.IncarcaSali(saliFisier);
+            cinema.IncarcaFilme(filmeFisier);
+
+            var utilizatori = File.Exists(utilizatoriFisier)
+                ? File.ReadAllLines(utilizatoriFisier).Select(Utilizator.FromString).ToList()
+                : new List<Utilizator>();
+
+            Utilizator utilizatorCurent = null;
 ﻿
