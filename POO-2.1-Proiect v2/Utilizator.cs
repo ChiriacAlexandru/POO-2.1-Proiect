487999ff054105e78e7727ea11c271635e4d3e34
ChiriacAlexandru <119078036+ChiriacAlexandru@users.noreply.github.com>
2025-01-15 19:57:41 +0200

    Update Utilizator.cs
    proiect final

--- POO-2.1-Proiect v2/Utilizator.cs
+++ POO-2.1-Proiect v2/Utilizator.cs
@@ -1,28 +1,31 @@
-﻿
-public class Utilizator
+namespace POO_2._1_Proiect_v2
 {
-    public string Nume;
-    public string Prenume;
-    public string Email;
-    public string Parola;
-    public bool IsAdmin;
-    public Utilizator(string nume, string prenume, string email,string parola,bool isAdmin)
+    public class Utilizator
     {
-        Nume= nume;
-        Prenume= prenume;
-        Email= email;
-        Parola = parola;
-        IsAdmin= isAdmin;
+        public string Nume { get; set; }
+        public string Prenume { get; set; }
+        public string Email { get; set; }
+        public string Parola { get; set; }
+        public bool IsAdmin { get; set; }
 
-    }
-    public override string ToString()
-    {
-        return $"{Nume},{Prenume},{Email},{Parola},{IsAdmin}";
-    }
-    public static Utilizator FromString(string line)
-    {
-        var parts = line.Split(',');
-        return new Utilizator(parts[0], parts[1], parts[2], parts[3], bool.Parse(parts[4]));
-    }
+        public Utilizator(string nume, string prenume, string email, string parola, bool isAdmin)
+        {
+            Nume = nume;
+            Prenume = prenume;
+            Email = email;
+            Parola = parola;
+            IsAdmin = isAdmin;
+        }
 
+        public override string ToString()
+        {
+            return $"{Nume},{Prenume},{Email},{Parola},{IsAdmin}";
+        }
+
+        public static Utilizator FromString(string line)
+        {
+            var parts = line.Split(',');
+            return new Utilizator(parts[0], parts[1], parts[2], parts[3], bool.Parse(parts[4]));
+        }
+    }
 }
