29d53f7abadf4be93ae9649784b379fa1fc2eb69
ChiriacAlexandru <119078036+ChiriacAlexandru@users.noreply.github.com>
2025-01-15 19:57:22 +0200

    Update Sala.cs
    proiect final

--- POO-2.1-Proiect v2/Sala.cs
+++ POO-2.1-Proiect v2/Sala.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace POO_2._1_Proiect_v2
 {
     public class Sala
     {
-        public int Id;
-        public int Capacitate;
+        public int Id { get; set; }
+        public int Capacitate { get; set; }
+
         public Sala(int id, int capacitate)
         {
             Id = id;
@@ -20,11 +15,11 @@ namespace POO_2._1_Proiect_v2
         {
             return $"{Id},{Capacitate}";
         }
+
         public static Sala FromString(string line)
         {
-            var parts=line.Split(',');
-            return new Sala(int.Parse(parts[0]),int.Parse(parts[1]));
+            var parts = line.Split(',');
+            return new Sala(int.Parse(parts[0]), int.Parse(parts[1]));
         }
-
     }
 }
