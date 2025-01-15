490a6d391f7fcd9bd533b78930afdab3cf245b99
ChiriacAlexandru <119078036+ChiriacAlexandru@users.noreply.github.com>
2025-01-15 19:56:20 +0200

    Update Film.cs
    proiect final

--- POO-2.1-Proiect v2/Film.cs
+++ POO-2.1-Proiect v2/Film.cs
@@ -1,25 +1,33 @@
-﻿public class Film
+using System;
+
+namespace POO_2._1_Proiect_v2
 {
-    public int Id;
-    public string Nume;
-    public DateTime DataStart;
-    public DateTime DataSfarsit;
-    public int SalaId;
-    public Film(int id, string nume, DateTime dataStart, DateTime dataSfarsit, int salaId)
+    public class Film
     {
-        Id = id;
-        Nume = nume;
-        DataStart = dataStart;
-        DataSfarsit = dataSfarsit;
-        SalaId = salaId;
-    }
-    public override string ToString()
-    {
-        return $"{Id},{Nume},{DataStart},{DataSfarsit},{SalaId}";
-    }
-    public static Film FromString(string line)
-    {
-        var parts = line.Split(',');
-        return new Film(int.Parse(parts[0]), parts[1], DateTime.Parse(parts[2]), DateTime.Parse(parts[3]), int.Parse(parts[4]));
+        public int Id { get; set; }
+        public string Nume { get; set; }
+        public DateTime DataStart { get; set; }
+        public DateTime DataSfarsit { get; set; }
+        public int SalaId { get; set; }
+
+        public Film(int id, string nume, DateTime dataStart, DateTime dataSfarsit, int salaId)
+        {
+            Id = id;
+            Nume = nume;
+            DataStart = dataStart;
+            DataSfarsit = dataSfarsit;
+            SalaId = salaId;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id},{Nume},{DataStart},{DataSfarsit},{SalaId}";
+        }
+
+        public static Film FromString(string line)
+        {
+            var parts = line.Split(',');
+            return new Film(int.Parse(parts[0]), parts[1], DateTime.Parse(parts[2]), DateTime.Parse(parts[3]), int.Parse(parts[4]));
+        }
     }
 }
